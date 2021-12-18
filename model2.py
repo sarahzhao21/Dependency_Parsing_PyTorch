b5a83d6ac5cb72146766967af9efa10a588d1ec6
sarahzhao21 <54957469+sarahzhao21@users.noreply.github.com>
2021-12-18 15:39:47 -0500

    Update model2.py

--- model2.py
+++ model2.py
@@ -22,7 +22,6 @@ class ParserModel(nn.Module):
         # Copy the Embedding data that we'll be using in the model.  Note that the
         # model gets these in the constructor so that the embeddings can come
         # from anywhere (the model is agnostic to the source of the embeddings).
-        # TODO
         self.word_embeddings = word_embeddings
         self.pos_embeddings = pos_embeddings 
         self.dep_embeddings = dep_embeddings
@@ -31,18 +30,13 @@ class ParserModel(nn.Module):
         # (consisting of embeddings of words, their corresponding POS tags, and
         # the arc labels) to the hidden layer raw outputs.
        
-        # TODO
         self.input = (n_w + n_p + n_d)* self.config.embedding_dim
         self.input_layer = nn.Linear(self.input, self.config.l1_hidden_size)
         
         # After the activation of the hidden layer, you'll be randomly zero-ing
         # out a percentage of the activations, which is a process known as
         # "Dropout".  Dropout helps the model avoid looking at the activation of
-        # one particular neuron and be more robust.  (In essence, dropout is
-        # turning the one network into an *ensemble* of networks).  Create a
-        # Dropout layer here that we'll use later in the forward() call.
-
-        # TODO
+        # one particular neuron and be more robust.  
         self.drop_out = nn.Dropout(p=0.2)
                 
         # Create the output layer that maps the activation of the hidden layer to
@@ -50,7 +44,6 @@ class ParserModel(nn.Module):
         self.input_layer2 = nn.Linear(self.config.l1_hidden_size, self.config.l1_hidden_size)
         self.input_layer3 = nn.Linear(self.config.l1_hidden_size, self.config.l1_hidden_size)
 
-        # TODO
         self.output_layer = nn.Linear(self.config.l1_hidden_size, self.config.num_classes)
 
         # Initialize the weights of both layers
@@ -71,7 +64,6 @@ class ParserModel(nn.Module):
         # Based on the IDs, look up the embeddings for each thing we need.  Note
         # that the indices are a list of which embeddings need to be returned.
 
-        # TODO
         w_embeddings = self.word_embeddings(torch.tensor(word_indices))
         p_embeddings = self.pos_embeddings(torch.tensor(pos_indices))
         d_embeddings = self.dep_embeddings(torch.tensor(dep_indices))
@@ -91,8 +83,6 @@ class ParserModel(nn.Module):
         # Look up the embeddings for this prediction.  Note that word_indices is
         # the list of certain words currently on the stack and buffer, rather
         # than a single word
-
-        # TODO
         w_embeddings, p_embeddings, d_embeddings = self.lookup_embeddings(word_indices, pos_indices, dep_indices)
 
         # Since we're converting lists of indices, we're getting a matrix back
@@ -103,12 +93,9 @@ class ParserModel(nn.Module):
         # dimensions per embedding, word_embeddings should be tensor with size
         # (2,3,5).  We need it to be a tensor with size (2,15), which makes the
         # input just like that flat input vector you see in the network diagram.
-        #
-        # HINT: you don't need to copy data here, only reshape the tensor.
-        # Functions like "view" (similar to numpy's "reshape" function will be
-        # useful here.        
+               
         #print("type or word_indices:", type(word_indices))
-        # TODO
+
         #print(word_indices.size()[0])
         word = w_embeddings.view(word_indices.size()[0], self.config.embedding_dim * self.config.word_features_types)
         pos = p_embeddings.view(pos_indices.size()[0], self.config.embedding_dim * self.config.pos_features_types)
@@ -121,18 +108,11 @@ class ParserModel(nn.Module):
         # Compute the raw hidden layer activations from the concatentated input
         # embeddings.
         #
-        # NOTE: if you're attempting the optional parts where you want to
-        # compute separate weight matrices for each type of input, you'll need
-        # do this step for each one!
-        
-        # TODO
         activation = self.input_layer(input_vector)
         ## activation is a tensor of (2048, 200)
         # Compute the cubic activation function here.
         #
-        # NOTE: Pytorch doesn't have a cubic activation function in the library
-
-        # TODO
+       
         # Original model
         activation_cubic = torch.tensor(np.power(activation.detach().numpy(), 3))
 
@@ -143,7 +123,7 @@ class ParserModel(nn.Module):
 
         # Now do dropout for final activations of the first hidden layer
 
-        # TODO
+
         #activation_drop = self.drop_out(activation_cubic)
         #activation_drop = self.drop_out(activation2_relu)
 
@@ -155,12 +135,7 @@ class ParserModel(nn.Module):
         # the second hidden layer and pass that through a ReLU non-linearity for
         # the final output activations.
         #
-        # NOTE 1: this output does not need to be pushed through a softmax if
-        # you're going to evaluate the output using the CrossEntropy loss
-        # function, which will compute the softmax intrinsically as a part of
-        # its optimization when computing the loss.
-
-        # TODO
+        
         activation_second = self.output_layer(activation_drop3)
         output = F.relu(activation_second)
         
